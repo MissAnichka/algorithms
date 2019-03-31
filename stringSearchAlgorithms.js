af33ac0497420e7f1869e2e5121981f6258b2ba4
MissAnichka <annamedyukh@gmail.com>
2019-03-31 17:35:03 -0400

    added general hash for rabin karp, cleaned up pattern search

--- stringSearchAlgorithms.js
+++ stringSearchAlgorithms.js
@@ -1,36 +1,47 @@
-// const needle = "abc"
-// const haystack = "aaabbbcccabcabaabccbadgfjedsabc"
-// [9, 15, 28]
+const needles = ["abc", "n e e d l e", "#$!*", "bbb"]
+const haystacks = ["aaabbbcccabcabaabccbadgfjedsabc", "i n a h a y s t a c k a n e e d l e i n a n e e d l e s a n d somuchmore!", "dsh#$!*aiehwfikjanc#$!*kafhjief!#$%^%#$!*$%&^#$!**&(", "bbbbb"]
+// index 0 answer = [ 9, 15, 28 ]
+// index 1 answer = [ 24, 42 ]
+// index 2 answer = [ 3, 19, 37, 45 ]
+// index 3 answer = [ 0, 1, 2 ]
 
-const needle = "n e e d l e"
-const haystack = "i n a h a y s t a c k a n e e d l e i n a n e e d l e s a n d somuchmore!"
-
+/**
+ * This is like Rabin Karp but optimizes a bit by jumping to 
+ * the next matching index in the string before hashing and checking
+ * @param {*} string 
+ * @param {*} substring 
+ */
 function patternSearch(string, substring) {
     const start = Date.now()
     const matches = [];
     const hashedSubString = hash(substring);
 
-    let i = string.indexOf(substring[0]), j = 0;
-    // console.log(i);
+    let i = string.indexOf(substring[0]);
 
-    while (i >= 0 && i <= string.length - substring.length && j >= 0 && j <= string.length - substring.length) {
-        let possibleMatch = string.slice(i, i + substring.length);
-        let hashedPossibleMatch = hash(possibleMatch);
+    while (i >= 0 && i <= string.length - substring.length) {
+        let hashedPossibleMatch = hash(string, i, i + substring.length);
         if (hashedPossibleMatch === hashedSubString) {
             matches.push(i);
         }
-        let uncheckedStr = string.slice(i + 1);
-        j = uncheckedStr.indexOf(substring[0]);
-        i = i + 1 + j;
-        // console.log(possibleMatch, hashedPossibleMatch, hashedSubString, i, j)
+        i = string.indexOf(substring[0], i + 1);
     }
     console.log(`*** Took ${Date.now() - start} milliseconds ***`);
     return matches;
 }
 
-console.log("My String Search Algo =", patternSearch(haystack, needle));
+console.log("My String Search Algo =", patternSearch(haystacks[0], needles[0]));
+console.log("My String Search Algo =", patternSearch(haystacks[1], needles[1]));
+console.log("My String Search Algo =", patternSearch(haystacks[2], needles[2]));
+console.log("My String Search Algo =", patternSearch(haystacks[3], needles[3]));
 
-function rabinKarp(string, substring) {
+/**
+ * Rabin Karp Algorithm - Returns first index within string of pattern passed in
+ * O(m*n) time complexity
+ * Read more here: https://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm
+ * @param {*} string 
+ * @param {*} substring 
+ */
+function rabinKarp2(string, substring) {
     const hashedSubString = hash(substring);
     for (let i = 0; i <= string.length - substring.length; i++) {
         let checkForMatch = hash(string, i, i + substring.length)
@@ -40,16 +51,51 @@ function rabinKarp(string, substring) {
     }
     return -1;
 }
-
+// using a unique hash
 function hash(str, left, right) {
     if(!left) left = 0;
     if(!right) right = str.length;
     let hashedNum = 0;
     for (let i = left, j = right-left; i < right; i++, j--) {
-        hashedNum += str.charCodeAt(i) * 128 ** j;
+        hashedNum += str.charCodeAt(i) * j;
     }
     return hashedNum;
 }
 
-console.log("Rabin Karp Algo =", rabinKarp(haystack, needle));
-// console.log("checking hash", hash("some n e e d l e", 5), hash("n e e d l e"))
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[0], needles[0]));
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[1], needles[1]));
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[2], needles[2]));
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[3], needles[3]));
+
+/**
+ * Rabin Karp using a more general hash
+ * @param {*} string 
+ * @param {*} substring 
+ */
+function rabinKarp1(string, substring) {
+    const hashedSubString = ezHash(substring);
+    for (let i = 0; i <= string.length - substring.length; i++) {
+        let checkForMatch = ezHash(string, i, i + substring.length)
+        if (checkForMatch === hashedSubString) {
+            if(string.slice(i, i + substring.length) === substring){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+function ezHash(str, left, right) {
+    if(!left) left = 0;
+    if(!right) right = str.length;
+    let hashedNum = 0;
+    for (let i = left, j = right-left; i < right; i++, j--) {
+        hashedNum += str.charCodeAt(i);
+    }
+    return hashedNum;
+}
+
+console.log("Rabin Karp easy hash =", rabinKarp1(haystacks[0], needles[0]));
+console.log("Rabin Karp easy hash =", rabinKarp1(haystacks[1], needles[1]));
+console.log("Rabin Karp easy hash =", rabinKarp1(haystacks[2], needles[2]));
+console.log("Rabin Karp easy hash =", rabinKarp1(haystacks[3], needles[3]));
