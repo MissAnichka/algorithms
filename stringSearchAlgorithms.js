10f44eb338e1f1ed593795ffa5b6ed19d9bf32d9
MissAnichka <annamedyukh@gmail.com>
2019-04-01 12:18:56 -0400

    organized logging

--- stringSearchAlgorithms.js
+++ stringSearchAlgorithms.js
@@ -1,10 +1,3 @@
-const needles = ["abc", "n e e d l e", "#$!*", "bbb"]
-const haystacks = ["aaabbbcccabcabaabccbadgfjedsabc", "i n a h a y s t a c k a n e e d l e i n a n e e d l e s a n d somuchmore!", "dsh#$!*aiehwfikjanc#$!*kafhjief!#$%^%#$!*$%&^#$!**&(", "bbbbb"]
-// index 0 answer = [ 9, 15, 28 ]
-// index 1 answer = [ 24, 42 ]
-// index 2 answer = [ 3, 19, 37, 45 ]
-// index 3 answer = [ 0, 1, 2 ]
-
 /**
  * This is like Rabin Karp but optimizes a bit by jumping to 
  * the next matching index in the string before hashing and checking
@@ -29,11 +22,6 @@ function patternSearch(string, substring) {
     return matches;
 }
 
-console.log("My String Search Algo =", patternSearch(haystacks[0], needles[0]));
-console.log("My String Search Algo =", patternSearch(haystacks[1], needles[1]));
-console.log("My String Search Algo =", patternSearch(haystacks[2], needles[2]));
-console.log("My String Search Algo =", patternSearch(haystacks[3], needles[3]));
-
 /**
  * Rabin Karp Algorithm - Returns first index within string of pattern passed in
  * O(m*n) time complexity
@@ -53,19 +41,14 @@ function rabinKarp2(string, substring) {
 }
 // using a unique hash
 function hash(str, left, right) {
-    if(!left) left = 0;
-    if(!right) right = str.length;
+    if (!left) left = 0;
+    if (!right) right = str.length;
     let hashedNum = 0;
-    for (let i = left, j = right-left; i < right; i++, j--) {
-        hashedNum += str.charCodeAt(i) * j;
+    for (let i = left, j = right - left; i < right; i++ , j--) {
+        hashedNum += str.charCodeAt(i) * 128 ** j;
     }
     return hashedNum;
 }
-
-console.log("Rabin Karp Algo =", rabinKarp2(haystacks[0], needles[0]));
-console.log("Rabin Karp Algo =", rabinKarp2(haystacks[1], needles[1]));
-console.log("Rabin Karp Algo =", rabinKarp2(haystacks[2], needles[2]));
-console.log("Rabin Karp Algo =", rabinKarp2(haystacks[3], needles[3]));
 
 /**
  * Rabin Karp using a more general hash
@@ -77,7 +60,7 @@ function rabinKarp1(string, substring) {
     for (let i = 0; i <= string.length - substring.length; i++) {
         let checkForMatch = ezHash(string, i, i + substring.length)
         if (checkForMatch === hashedSubString) {
-            if(string.slice(i, i + substring.length) === substring){
+            if (string.slice(i, i + substring.length) === substring) {
                 return i;
             }
         }
@@ -86,14 +69,40 @@ function rabinKarp1(string, substring) {
 }
 
 function ezHash(str, left, right) {
-    if(!left) left = 0;
-    if(!right) right = str.length;
+    if (!left) left = 0;
+    if (!right) right = str.length;
     let hashedNum = 0;
-    for (let i = left, j = right-left; i < right; i++, j--) {
+    for (let i = left, j = right - left; i < right; i++ , j--) {
         hashedNum += str.charCodeAt(i);
     }
     return hashedNum;
 }
+
+const needles = [
+    "abc",
+    "n e e d l e",
+    "#$!*",
+    "bbb"
+]
+const haystacks = [
+    "aaabbbcccabcabaabccbadgfjedsabc",
+    "i n a h a y s t a c k a n e e d l e i n a n e e d l e s a n d somuchmore!", "dsh#$!*aiehwfikjanc#$!*kafhjief!#$%^%#$!*$%&^#$!**&(",
+    "bbbbb"
+]
+
+console.log("My String Search Algo =", patternSearch(haystacks[0], needles[0]));
+// index 0 answer = [ 9, 15, 28 ]
+console.log("My String Search Algo =", patternSearch(haystacks[1], needles[1]));
+// index 1 answer = [ 24, 42 ]
+console.log("My String Search Algo =", patternSearch(haystacks[2], needles[2]));
+// index 2 answer = [ 3, 19, 37, 45 ]
+console.log("My String Search Algo =", patternSearch(haystacks[3], needles[3]));
+// index 3 answer = [ 0, 1, 2 ]
+
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[0], needles[0]));
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[1], needles[1]));
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[2], needles[2]));
+console.log("Rabin Karp Algo =", rabinKarp2(haystacks[3], needles[3]));
 
 console.log("Rabin Karp easy hash =", rabinKarp1(haystacks[0], needles[0]));
 console.log("Rabin Karp easy hash =", rabinKarp1(haystacks[1], needles[1]));
